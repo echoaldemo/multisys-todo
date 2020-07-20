df7d681cf437a652fded0345e427c3117c0b8afd
Jericho Aldemo <jericho.aldemo@boom.camp>
2020-07-20 17:52:19 +0800

    Added mock data to initial state for testing

--- src/redux/reducers/tasks.js
+++ src/redux/reducers/tasks.js
@@ -1,5 +1,12 @@
 import { v4 as uuidv4 } from "uuid";
-const taskReducer = (state = [], action) => {
+
+const initialState = [
+  { id: uuidv4(), desc: "Go for a jog.", status: false },
+  { id: uuidv4(), desc: "Go to the market.", status: false },
+  { id: uuidv4(), desc: "Bathe the dogs.", status: true },
+];
+
+const taskReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD":
       const newTask = {
