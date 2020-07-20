a5ae06a5c52e812a79b1e7ed6b1642e418184ce3
Jericho Aldemo <jericho.aldemo@boom.camp>
2020-07-20 17:04:10 +0800

    Added redux, bootstrap and uuid to dependencies

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,6 +6,26 @@ const Todo = () => {
       <div className="container">
         <h3 className="header">To Do App</h3>
         <h6 className="sub">by Jericho Aldemo</h6>
+        <div>
+          <form className="input-container">
+            <input
+              width="485px"
+              //   onChange={(e) =>
+              //     dispatch(allActions.inputActions.setTask(e.target.value))
+              //   }
+              //   value={input.task}
+              className="form-control"
+              placeholder="Enter a task for today"
+            />
+            <button
+              //disabled={input.task.length === 0}
+              className="btn btn-primary"
+              type="submit"
+            >
+              Add Task
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
