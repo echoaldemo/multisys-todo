3ddf1873d1da441069d1ae79d38ed8d35715bc0f
Jericho Aldemo <jericho.aldemo@boom.camp>
2020-07-20 16:59:32 +0800

    Added redux folders and initial Todo component

---
+++ src/components/ToDo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Todo = () => {
+  return (
+    <div className="main">
+      <div className="container">
+        <h3 className="header">To Do App</h3>
+        <h6 className="sub">by Jericho Aldemo</h6>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
