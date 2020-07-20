4d52da04758dc72f344c9debb0bc0447bf421c2f
Jericho Aldemo <jericho.aldemo@boom.camp>
2020-07-20 19:35:44 +0800

    Updates

---
+++ precache-manifest.4ceef4404cc18208d6785bebd517aaa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70357cdfc8a2220c00b6811c2cc36fd5",
+    "url": "/multisys-todo/index.html"
+  },
+  {
+    "revision": "fa9da7f953be0da89cac",
+    "url": "/multisys-todo/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "768c95680f66d0c7dd0f",
+    "url": "/multisys-todo/static/css/main.dd7b5645.chunk.css"
+  },
+  {
+    "revision": "fa9da7f953be0da89cac",
+    "url": "/multisys-todo/static/js/2.fd2194fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/multisys-todo/static/js/2.fd2194fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768c95680f66d0c7dd0f",
+    "url": "/multisys-todo/static/js/main.4c0bc0d7.chunk.js"
+  },
+  {
+    "revision": "56248b90d5b24ec5e93a",
+    "url": "/multisys-todo/static/js/runtime-main.40af03c0.js"
+  }
+]);
